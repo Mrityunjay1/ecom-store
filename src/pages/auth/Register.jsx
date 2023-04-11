59b17ba5420898fe431302bacb03e97028fe6c79
Mrityunjay1 <singhmrityunjay32@gmail.com>
2023-04-11 11:07:07 +0530

    added login and registration logic

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,34 +1,88 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./auth.module.scss";
 import regImg from "../../assets/register.png";
 import Card from "../../components/card/Card";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
+import Loader from "../../components/loader/Loader";
+import { auth } from "../../firebase/config";
 
 const Register = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [cpassword, setCpassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const registerUser = (e) => {
+    e.preventDefault();
+    if (password !== cpassword) {
+      toast.error("Password dont match");
+    }
+    setIsLoading(true);
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user);
+        setIsLoading(false);
+        toast.success("Account created Successfully");
+        navigate("/login");
+        // ...
+      })
+      .catch((error) => {
+        const errorMessage = error.message;
+        toast.error(errorMessage);
+        setIsLoading(false)
+      });
+  };
   return (
-    <section className={`container ${styles.auth}`}>
-      <Card>
-        <div className={styles.form}>
-          <h2>Register</h2>
+    <>
+      <ToastContainer />
+      {isLoading && <Loader />}
+      <section className={`container ${styles.auth}`}>
+        <Card>
+          <div className={styles.form}>
+            <h2>Register</h2>
 
-          <form action="">
-            <input type="text" placeholder="Email" required />
-            <input type="password" placeholder="Password" required />
-            <input type="password" placeholder="Confirm Password" required />
-            <button className="--btn --btn-primary --btn-block">
-              Register
-            </button>
-          </form>
-          <span className={styles.register}>
-            <p>Already have a account? &nbsp;</p>
-            <Link to="/login">Login</Link>
-          </span>
+            <form onSubmit={registerUser}>
+              <input
+                type="text"
+                placeholder="Email"
+                value={email}
+                required
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <input
+                type="password"
+                placeholder="Confirm Password"
+                required
+                value={cpassword}
+                onChange={(e) => setCpassword(e.target.value)}
+              />
+              <button type="submit" className="--btn --btn-primary --btn-block">
+                Register
+              </button>
+            </form>
+            <span className={styles.register}>
+              <p>Already have a account? &nbsp;</p>
+              <Link to="/login">Login</Link>
+            </span>
+          </div>
+        </Card>
+        <div className={styles.img}>
+          <img src={regImg} width="400px" alt="Login" />
         </div>
-      </Card>
-      <div className={styles.img}>
-        <img src={regImg} width="400px" alt="Login" />
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
